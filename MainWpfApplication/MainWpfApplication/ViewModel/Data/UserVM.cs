06fe639d4dae9cf3cf3c829eb1b93300080f5509
Андрей Рак <rao333@yandex.ru>
2016-02-26 00:51:22 +0200

    Reload again

---
+++ MainWpfApplication/MainWpfApplication/ViewModel/Data/UserVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainWpfApplication.ViewModel.Data
+{
+    public class UserVM : ViewModelBase
+    {
+        private UserType _user;
+        private TrainerVM _trainerVm;
+
+    }
+}
